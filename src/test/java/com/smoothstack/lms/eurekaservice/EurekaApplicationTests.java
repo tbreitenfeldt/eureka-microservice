a77e7d82541a8070cedfbce4b9741c4a30c2b2e6
tbreitenfeldt <timothyjb310@gmail.com>
2020-09-05 20:26:58 -0400

    Initial commit.

---
+++ src/test/java/com/smoothstack/lms/eurekaservice/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.lms.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
